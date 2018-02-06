ae711c5200284391f9e3168e2b47bb07fce0f12e
Deniise Adrianne Palad <paladdenise17@gmail.com>
2018-02-06 17:36:08 +0800

    Exam Timer Updated

--- AndersonExamWeb/Scripts/Angular/Controller/ExamTimer.js
+++ AndersonExamWeb/Scripts/Angular/Controller/ExamTimer.js
@@ -40,16 +40,18 @@ function addLoadEvent(func) {
 
     var oldonload = window.onload;
     if (typeof window.onload != 'function') {
-        
+        window.onload = func;
+
     } else {
         window.onload = function () {
             if (oldonload) {
                 oldonload();
-              
-            }    
+            }
+            func();
+        }
     }
-}
 
-addLoadEvent(function () {
-    countDown();
-});
+    addLoadEvent(function () {
+        countDown();
+    });
+}
